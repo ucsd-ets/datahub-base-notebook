cbda69e1e2a9f713ac847c858a0c02c008f3b587
Wesley Uykimpang <wuykimpang@ucsd.edu>
2019-08-13 09:43:26 -0700

    changed install-nbresuse.sh to point to latest nbresuse repo

--- install-nbresuse.sh
+++ install-nbresuse.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -x
-pip install --no-cache-dir git+https://github.com/agt-ucsd/nbresuse.git && \
+pip install --no-cache-dir git+https://github.com/ucsd-ets/nbresuse.git && \
 	jupyter serverextension enable --sys-prefix --py nbresuse && \
 	jupyter nbextension install --sys-prefix --py nbresuse && \
 	jupyter nbextension enable --sys-prefix --py nbresuse

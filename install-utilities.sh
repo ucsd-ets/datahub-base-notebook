d3b096c1f319aa7b64ce328bdd2b6aef7b956998
David Andersen <dta001@ucsd.edu>
2019-08-13 16:40:54 +0000

    install git

--- install-utilities.sh
+++ install-utilities.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 apt-get update && \
   apt-get -qq install -y \
-  	curl \
-  	rsync \
-  	unzip \
-  	less \
+    git \
+    curl \
+    rsync \
+    unzip \
+    less \
     nano \
     vim \
-  	openssh-client \
-		cmake \
-		tmux \
-		screen \
-		gnupg \
-  	wget && \
+    openssh-client \
+    cmake \
+    tmux \
+    screen \
+    gnupg \
+    wget && \
 	chmod g-s /usr/bin/screen && \
 	chmod 1777 /var/run/screen

78861e930be77583b5324a898957b4111d85138d
David Andersen <dta001@ucsd.edu>
2019-08-13 16:41:10 +0000

    add debug

--- install-nbresuse.sh
+++ install-nbresuse.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -x
 pip install --no-cache-dir git+https://github.com/agt-ucsd/nbresuse.git && \
 	jupyter serverextension enable --sys-prefix --py nbresuse && \
 	jupyter nbextension install --sys-prefix --py nbresuse && \

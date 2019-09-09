4e665cbdffab59cb863a6bc5d1b0a421e00d71e1
Wesley Uykimpang <wuykimpang@ucsd.edu>
2019-09-09 14:37:55 -0700

    reedited cluster status

--- install-nbcluster-status.sh
+++ install-nbcluster-status.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -x
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>nbcluster.out 2>&1
+
 pip install --no-cache-dir git+https://github.com/ucsd-ets/nbcluster-status.git && \
-	jupyter serverextension enable --sys-prefix --py nbcluster_status && \
-	jupyter nbextension install --sys-prefix --py nbcluster_status && \
-	jupyter nbextension enable --sys-prefix --py nbcluster_status
+	jupyter serverextension enable --py nbcluster_status --sys-prefix && \
+	jupyter nbextension install --py nbcluster_status --sys-prefix && \
+	jupyter nbextension enable --py nbcluster_status --sys-prefix

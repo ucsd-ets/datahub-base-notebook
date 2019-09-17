d9d607ce71e8b1bc1afc1fc614b46f6c4528e9c0
Wesley Uykimpang <wuykimpang@ucsd.edu>
2019-09-17 14:57:32 -0700

    removed logfile from install-nbcluster-status.sh

--- install-nbcluster-status.sh
+++ install-nbcluster-status.sh
@@ -1,7 +1,4 @@
 #!/bin/sh -x
-exec 3>&1 4>&2
-trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>nbcluster.out 2>&1
 
 pip install --no-cache-dir git+https://github.com/ucsd-ets/nbcluster-status.git && \
 	jupyter serverextension enable --py nbcluster_status --sys-prefix && \

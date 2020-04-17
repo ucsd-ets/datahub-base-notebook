44e901a4a4b7f62449d71e1c7e4d89dff82fc002
Wesley Uykimpang <wuykimpang@ucsd.edu>
2020-04-17 10:27:51 -0700

    renamed test notebook

--- tests/test.sh
+++ tests/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 DATASCIENCE_TESTDIR=$TESTDIR/datahub-base-notebook
-jupyter nbconvert --to python "${DATASCIENCE_TESTDIR}/datascience_notebook.ipynb"
+jupyter nbconvert --to python "${DATASCIENCE_TESTDIR}/datahub-base-notebook.ipynb"
 
 
-if ! python3 ${DATASCIENCE_TESTDIR}/datascience_notebook.py; then
+if ! python3 ${DATASCIENCE_TESTDIR}/datahub-base-notebook.py; then
     exit 1
 fi
 

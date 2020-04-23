903ee85e96782c2ceb2d82ceabf9d2b8d9cf7c04
Wesley Uykimpang <wuykimpang@ucsd.edu>
2020-04-23 11:14:48 -0700

    Removed ok test, for ucsdets/datscience-notebook, not base

--- tests/test.sh
+++ tests/test.sh
@@ -12,8 +12,3 @@ fi
 protoc -I=$DATASCIENCE_TESTDIR --python_out=$DATASCIENCE_TESTDIR $DATASCIENCE_TESTDIR/addressbook.proto
 python3 $DATASCIENCE_TESTDIR/addressbook_pb2.py
 
-# test okpy
-git clone https://github.com/okpy/ok-client.git
-ok -q ok-client/demo/ok_test/q2
-rm -rf ok-client
-

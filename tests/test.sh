1c0bb2b45310dd2f4a8774ba72b3b9b8e3d4666e
Yuxin Zou <12762985+davidzyx@users.noreply.github.com>
2020-08-06 14:08:59 -0700

    Added local-env-backup

--- tests/test.sh
+++ tests/test.sh
@@ -5,6 +5,9 @@ DATASCIENCE_TESTDIR=$TESTDIR/datahub-base-notebook
 ### Add any non-selenium tests here
 python3 -m pip install pytest
 pytest $DATASCIENCE_TESTDIR/test-ipykernel_clean.py -v
+pip uninstall multiprocess -y
+pytest /usr/share/datahub/local-env-backup/tests/cli-test.py -v
+pip install multiprocess
 
 # test the datahub-base-notebook.py without opening it as an ipynb notebook
 jupyter nbconvert --to python "${DATASCIENCE_TESTDIR}/datahub-base-notebook.ipynb"
